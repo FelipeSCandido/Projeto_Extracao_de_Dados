a20e175553b0a660f1702f9bd6abf7a5006ff952
Felipe Sant'ana Candido <felipe.candido@ubi.pt>
2026-05-11 16:36:40 +0100

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -3,34 +3,26 @@ import requests
 import json
 from dotenv import load_dotenv
 
-# 1. Configuração de Variáveis de Ambiente [cite: 90]
-# Carrega a API Key do teu ficheiro .env para garantir a segurança das credenciais [cite: 155, 179]
 load_dotenv()
 API_KEY = os.getenv("RAWG_API_KEY")
 
 def extract_games_data(page_size=40):
-    """
-    Realiza a extração de metadados de jogos da API RAWG.
-    Esta função cumpre o requisito de extração modular e logging mínimo[cite: 193, 203].
-    """
-    print(f"--- Iniciando Módulo de Extração (Semana 1) ---")
+
+    print(f"Módulo de Extração")
     
     if not API_KEY:
         print("Erro: RAWG_API_KEY não encontrada no ficheiro .env")
         return
 
-    # Endpoint da API RAWG para listar jogos populares
     url = f"https://api.rawg.io/api/games?key={API_KEY}&page_size={page_size}"
     
     try:
-        # Realizar o pedido à API [cite: 77]
         response = requests.get(url)
-        response.raise_for_status() # Verifica se a requisição foi bem-sucedida
+        response.raise_for_status()
         data = response.json()
         
-        # 2. Persistência de Dados Brutos (Camada Bronze/Raw) [cite: 202]
-        # Os dados são guardados exatamente como chegam, sem transformações [cite: 195, 202]
-        os.makedirs("data/raw", exist_ok=True) # Garante que a pasta existe [cite: 201]
+      
+        os.makedirs("data/raw", exist_ok=True)
         output_path = "data/raw/games_metadata_raw.json"
         
         with open(output_path, "w", encoding="utf-8") as f:
